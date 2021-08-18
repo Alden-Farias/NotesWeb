02277cbf7e861c302b9ec2445e05880fbba66b45
Joel <joellovesa380@gmailcom>
2021-08-18 23:56:18 +0530

    Added animations to new note popup

--- Notes/index.js
+++ Notes/index.js
@@ -1,6 +1,6 @@
 var newNoteOpen = false
 
-$("#fab-add-note").click(function (e) { 
+$("#fab-add-note").click(function (e) {
     $("#add-note-dialog-notes").css("display", "block")
     newNoteOpen = true
 });
@@ -8,9 +8,32 @@ $("#fab-add-note").click(function (e) {
 $(window).on("navigate", function (event, data) {
     var direction = data.state.direction;
     if (direction == 'back') {
-      // do something
+        // do something
     }
     if (direction == 'forward') {
-      // do something else
+        // do something else
     }
-  });
+});let newNoteOpen = false
+
+$("#fab-add-note").click(function (e) {
+    $("#add-note-dialog-notes").css("display", "block")
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+    sleep(0).then(() => {
+        $("#card-dialog").addClass("show");
+        $("#add-note-dialog-notes").addClass("show");
+    });
+    newNoteOpen = true
+});
+
+$(window).on("navigate", function (event, data) {
+    let direction = data.state.direction;
+    if (direction == 'back') {
+        if (newNoteOpen)
+            $("#add-note-dialog-notes").css("display", "none")
+    }
+    if (direction == 'forward') {
+        // do something else
+    }
+});
